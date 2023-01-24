5b777144da4fd8101d0b3dc3f5aa9a095c376c8b
Henri Uimonen <95289841+Henriui@users.noreply.github.com>
2023-01-24 09:05:50 +0200

    Bonus

--- AbstractFactory/Main.java
+++ AbstractFactory/Main.java
@@ -1,12 +1,31 @@
 package AbstractFactory;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import AbstractFactory.AdidasFactory.AdidasFactory;
 import AbstractFactory.BossFactory.BossFactory;
 
 public class Main {
     public static void main(String[] args) {
         // Jesper on main
-        VaateFactory vaatteet = new AdidasFactory();
+
+        Class c = null;
+        VaateFactory vaatteet = null;
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileInputStream("AbstractFactory/tehdas.properties"));
+        } catch (IOException e) {e.printStackTrace();}
+        try{
+            c = Class.forName(properties.getProperty("tehdas"));
+            vaatteet = (VaateFactory)c.getDeclaredConstructor().newInstance();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // VaateFactory vaatteet = new AdidasFactory();
         // VaateFactory vaatteet = new BossFactory();
         Kengat kengat = vaatteet.LuoKengat();
         Tpaita tpaita = vaatteet.luoTpaita();
