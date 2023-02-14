1bfb5fa2d51b2937f327c1b863e98c1db1e51664
Henri Uimonen <henri.uimonen@gmail.com>
2023-02-14 11:30:34 +0200

    Add example of non-decorated FileIO

--- Decorator/Main.java
+++ Decorator/Main.java
@@ -1,17 +1,23 @@
 package Decorator;
 
 public class Main {
+    // Test variables.
+    private static String test1 = "test1.txt", test2 = "test2.txt", testString = "Hello World";
+
     public static void main(String[] args) {
+        // Create an implementation of FileIO and read + write to file without encryption
+
+        FileIO fileIOimpl = new FileIOImpl();
+        fileIOimpl.writeToFile(test1, testString);
+        String contentimpl = fileIOimpl.readFromFile(test1);
+        System.out.println("Content: " + contentimpl);
+
         // Create an implementation of FileIO and decorate it with FileEncrypter.
+        // Write Hello World to file encrypted and read it back decrypted.
+
         FileIO fileIO = new FileEncrypter(new FileIOImpl());
-
-        // Write Hello World to file.
-        fileIO.writeToFile("test.txt", "Hello World");
-
-        // Read it from file.
-        String content = fileIO.readFromFile("test.txt");
-
-        // Print the content.
+        fileIO.writeToFile(test2, testString);
+        String content = fileIO.readFromFile(test2);
         System.out.println("Content: " + content);
     }
 }
