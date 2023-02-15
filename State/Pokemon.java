920fe871c0bdbd93f5ab290abcc504696f3cb5c6
Henri Uimonen <henri.uimonen@gmail.com>
2023-02-15 13:01:49 +0200

    extract main into Main

--- State/Pokemon.java
+++ State/Pokemon.java
@@ -24,15 +24,5 @@ public class Pokemon {
         evolution.defend();
     }
 
-    public static void main(String[] args) {
-        Pokemon pokemon = new Pokemon();
-
-        for (int i = 0; i < 3; i++) {
-            pokemon.attack();
-            pokemon.specialAttack();
-            pokemon.defend();
-            pokemon.evolvePokemon();
-        }
-    }
 
 }
