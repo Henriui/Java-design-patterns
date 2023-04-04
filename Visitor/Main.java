548520e4d6325b4240dd41bda9fbe3b27b3e06a8
henriui <henri.uimonen@gmail.com>
2023-04-04 17:52:15 +0300

    s

--- Visitor/Main.java
+++ Visitor/Main.java
@@ -3,7 +3,7 @@ package Visitor;
 public class Main {
     public static void main(String[] args) {
         Pokemon pokemon = new Pokemon();
-        for (int i = 0; i <10; i++) {
+        for (int i = 0; i <4; i++) {
             pokemon.attack();
             pokemon.specialAttack();
             pokemon.defend();
